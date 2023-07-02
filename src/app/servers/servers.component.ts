3ba7440288e45d0460628e94a69b4f8aedfc21c1
LucasFA94 <lucasfernandesafonso@gmail.com>
2023-07-02 20:18:16 -0300

    Passing Data

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ export class ServersComponent implements OnInit{
 
   allowNewServer: boolean = false;
   serverCreationStatus: string = "No server was created";
+  serverName = '';
+
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +25,13 @@ export class ServersComponent implements OnInit{
   onCreateServer() {
     this.serverCreationStatus= "Server was created"
   }
+
+  onUpdateServerName(event: any) {
+
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+
 
 }
 function ngOnInit() {
